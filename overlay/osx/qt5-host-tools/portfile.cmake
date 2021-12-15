9af5e403e19a3b92b4ea064bf333dc244d97f6ab
Daniel Schürmann <daschuer@mixxx.org>
2021-12-15 12:51:19 +0100

    [qt5] added a qt5-host-tools helper package for exporting the host tools

---
+++ overlay/osx/qt5-host-tools/portfile.cmake
@@ -0,0 +1,10 @@
+
+if ("${TARGET_TRIPLET}" STREQUAL ${HOST_TRIPLET}) 
+    # first path 
+    # This creates packages/qt5-host-tools_${TARGET_TRIPLET}
+    message(STATUS "creating a package form ${CURRENT_HOST_INSTALLED_DIR}/tools/qt5")
+        file(COPY "${CURRENT_HOST_INSTALLED_DIR}/tools/qt5/" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/qt5/")
+else()
+    # crosscompiling 
+    message(FATAL_ERROR "qt5-host-tools can only be installed for the host")
+endif()
